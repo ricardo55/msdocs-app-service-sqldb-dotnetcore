693514987a50e54149d85f9921338dbf750f0d5b
Ricardo VB <pegassoric@hotmail.com>
2023-12-26 02:24:47 -0600

    Cambie la ruta para ver funcionamiento

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -70,7 +70,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Todos}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
